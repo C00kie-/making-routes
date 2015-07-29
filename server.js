d3312b09632cf3fa6d893c4287dc76e704003622
C00kie- <pbourmea@student.42.fr>
2015-07-29 16:16:59 +0200

    first server module

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http')
 
-http.createServer(function(req, res){
+function start(){
+function onRequest (req, res){
+	console.log('Request received.');
 	res.writeHead(200, {'Content-Type':'text/plain'});
 	res.write('Hello World');
 	res.end();
-}).listen(8888);
+	}
+	http.createServer (onRequest).listen(8888);
+	console.log('lancement du serveur, listen to 8888');
+}
+
+exports.start = start;
