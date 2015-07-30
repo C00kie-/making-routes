1e3bd3ccc244d84433b70adf1f42d94fa22cdd57
C00kie- <pbourmea@student.42.fr>
2015-07-30 13:58:48 +0200

    router.js

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
-function route(pathname){
+function route(handle, pathname){
 	console.log('debut du traitement de l url' + pathname + '.');
+	if (typeof handle[pathname] === 'function'){
+		handle[pathname]();
+	} else {
+		console.log('aucun gestionnaire associe a' + pathname);
+	}
 }
 
 exports.route = route;
