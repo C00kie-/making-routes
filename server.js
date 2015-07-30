28e6ac33c11c711836f195ae47e89dcd805b5c41
C00kie- <pbourmea@student.42.fr>
2015-07-30 13:38:09 +0200

    add route(handle, pathname)

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var http = require('http')
 var url = require('url')
 
-function start(route){
+function start(route, handle){
 function onRequest (req, res){
 	var pathname = url.parse(req.url).pathname;
 	console.log('Request received for ' + pathname + '.');
-	route(pathname);
+	route(handle, pathname);
 	res.writeHead(200, {'Content-Type':'text/plain'});
 	res.write('Hello World');
 	res.end();
