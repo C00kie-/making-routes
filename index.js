23b68a0512451e8d6ba752b1f41d3709a30256ca
C00kie- <pbourmea@student.42.fr>
2015-07-30 13:35:53 +0200

    making requestHandlers

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+var handle = {}; /*un nvl obj, pas un peu sale?*/
 
-server.start(router.route);
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+/*fin de la def de l obj*/
+
+server.start(router.route, handle);
+
---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start() {
+	  console.log("Le gestionnaire 'start' est appelé.");
+}
+
+function upload() {
+	  console.log("Le gestionnaire 'upload' est appelé.");
+}
+
+exports.start = start;
+exports.upload = upload;
